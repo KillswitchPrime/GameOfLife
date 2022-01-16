3ec58e6538dad98c0243aeca5333271afd41d87c
Christopher I. Jamil <flip_d_@hotmail.com>
2022-01-16 13:20:45 +0100

    Added cell enum

---
+++ GameOfLife/CellEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace GameOfLife
+{
+    internal enum CellEnum
+    {
+        NorthWest,
+        North,
+        NorthEast,
+        West,
+        East,
+        SouthWest,
+        South,
+        SouthEast
+    }
+}
